ee9eb58b92987b34b7d572b805858112e4dac544
somabalazsi <46135681+somabalazsi@users.noreply.github.com>
2018-12-31 14:23:31 +0100

    first release

--- main.ts
+++ main.ts
@@ -1,7 +1,152 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Clock functions
+ */
+//% weight=100 color=#00cc96 icon="\uf017" block="Clock"
+namespace clock {
+    let timeoffset = 0
+    let timecounter = 0
+    let Hour = 0
+    let Minute = 0
+    let Second = 0
+    let ampm = false
+
+    /**
+     * Returns the time as a string in the format "00:00"
+     */
+    //% block
+    export function stringTime(): string {
+        let stringtime = ""
+        Clock()
+        if (Minute < 10) {
+            stringtime = "0" + Minute
+        } else {
+            stringtime = "" + Minute
+        }
+        if (Hour < 10) {
+            stringtime = "0" + Hour + ":" + stringtime
+        } else {
+            stringtime = "" + Hour + ":" + stringtime
+        }
+        return stringtime
+    }
+
+    /**
+     * Only returns the hours of the time
+     */
+    //% block
+    export function getHour(): number {
+        Clock()
+        return Hour
+    }
+
+    /**
+     * Only returns the minutes of the time
+     */
+    //% block
+    export function getMinute(): number {
+        Clock()
+        return Minute
+    }
+
+    /**
+     * Only returns the seconds of the time
+     */
+    //% block
+    export function getSecond(): number {
+        Clock()
+        return Second
+    }
+
+    /**
+     * Returns the time in seconds
+     */
+    //% block
+    export function getTimeInSeconds(): number {
+        Clock()
+        return Math.floor(input.runningTime() / 1000) + timeoffset
+    }
+
+    /**
+     * Sets the time to a specific value 
+     * eg: 11, 54, 23 is 11:54:23
+     * ignores invalid time
+     */
+    //% block
+    export function SetTime(hour: number, minute: number, second: number = 0): void {
+        Clock()
+        let time = Math.floor(input.runningTime() / 1000) + timeoffset
+        Hour = Math.floor(time / 3600)
+        Minute = Math.floor((time - Hour * 3600) / 60)
+        Second = time - Hour * 3600 - Minute * 60
+
+        if (!(hour >= 0 && hour < 24 && minute >= 0 && minute < 60 && second >= 0 && second < 60)) {
+            return
+        }
+
+        timeoffset -= (Hour - hour) * 3600
+        timeoffset -= (Minute - minute) * 60
+        timeoffset -= (Second - second)
+
+        Clock()
+    }
+
+    /**
+     * Enables or disables the 12 hour clock
+     */
+    //% block
+    export function EnableAmPm(value: boolean) {
+        ampm = value
+    }
+
+    function Clock(): void {
+        timecounter++
+        if (timecounter >= 3600) {
+            timecounter = 0
+            timeoffset += 30
+        }
+        let time = Math.floor(input.runningTime() / 1000) + timeoffset
+        if (ampm) {
+            if (time >= 12 * 60 * 60) {
+                timeoffset -= 24 * 60 * 60
+            }
+        } else {
+            if (time >= 24 * 60 * 60) {
+                timeoffset -= 24 * 60 * 60
+            }
+        }
+        time = Math.floor(input.runningTime() / 1000) + timeoffset
+        Hour = Math.floor(time / 3600)
+        Minute = Math.floor((time - Hour * 3600) / 60)
+        Second = time - Hour * 3600 - Minute * 60
+    }
+
+    /**
+     * Adds x hours to the time
+     * if argument left empty, it will add 1
+     */
+    //% block
+    export function AddHour(hour: number = 1): void {
+        timeoffset += hour * 3600
+        Clock()
+    }
+
+    /**
+     * Adds x minutes to the time
+     * if argument left empty, it will add 1
+     */
+    //% block
+    export function AddMinute(minute: number = 1): void {
+        timeoffset += minute * 60
+        Clock()
+    }
+
+	/**
+     * Adds x seconds to the time
+     * if argument left empty, it will add 1
+     */
+    //% block
+    export function AddSecond(second: number = 1): void {
+        timeoffset += second
+        Clock()
+    }
+}
